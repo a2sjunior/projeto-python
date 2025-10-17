5a240db89d7a7fee8beab60bc2bf95aa403db62b
Antonio Jr <a2sjunior@gmail.com>
2025-10-17 10:09:11 -0300

    Adiciona uma questão

--- aula25.py
+++ aula25.py
@@ -32,6 +32,7 @@ Exercícios
 Crie um programa que leia o nome e a idade de uma pessoa e exiba as seguintes informações
 Seu nome é {nome}
 Seu nome invertido é {nome invertido}
+Seu nome contém espaços ou Seu nome não contém espaços
 Seu nome tem {n} letras
 A primeira letra do seu nome é {primeira letra}
 A última letra do seu nome é {última letra}
@@ -39,17 +40,23 @@ A última letra do seu nome é {última letra}
 Se nada for informado em nome ou idade exiba uma mensagem dizendo que os dados são obrigatórios e finalize o programa.
 """
 
-# nome = input("Digite seu nome: ")
-# idade = input("Digite sua idade: ")
+nome = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
 
-# if nome and idade:
-#     print(f"Seu nome é {nome}")
-#     print(f"Seu nome invertido é {nome[::-1]}")
-#     print(f"Seu nome tem {len(nome)} letras")
-#     print(f"A primeira letra do seu nome é {nome[0]}")
-#     print(f"A última letra do seu nome é {nome[-1]}")
-# else:
-#     print("Dados obrigatórios!")
+if nome and idade:
+    print(f"Seu nome é {nome}")
+    print(f"Seu nome invertido é {nome[::-1]}")
+    
+    if ' ' in nome:
+        print("Seu nome contém espaços")
+    else:
+        print("Seu nome não contém espaços")
+
+    print(f"Seu nome tem {len(nome)} letras")
+    print(f"A primeira letra do seu nome é {nome[0]}")
+    print(f"A última letra do seu nome é {nome[-1]}")
+else:
+    print("Dados obrigatórios!")
 
 # if not nome or not idade:
 #     print("Dados obrigatórios!")
